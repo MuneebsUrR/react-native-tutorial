56614f37ad39e08f37013a18f1c9aa14f3e752b4
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-07-10 23:11:04 +0500

    platform specific code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, Dimensions, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import { Button, Dimensions, Platform, StyleSheet, Text, useWindowDimensions, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -8,37 +8,52 @@ export default function App() {
   Wwidth = useWindowDimensions().width;
   Wheight = useWindowDimensions().height
   return (
+    <>
 
+      <StatusBar style={Platform.OS === 'android' ? 'light' : 'dark'} />
 
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.safeContainer}>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-          <View
-            style={[
-              styles.box,
-              {
-                width: Wwidth > 400 ? '90%' : '60%',
-                height: Wheight > 400 ? '70%' : '50%',
-              },
-            ]}
-          >
-            <Text>Welcome!</Text>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.safeContainer}>
+          <View style={styles.container}>
+
+            <View
+              style={[
+                styles.box,
+                {
+                  width: Wwidth > 400 ? '90%' : '60%',
+                  height: Wheight > 400 ? '70%' : '50%',
+                },
+              ]}
+            >
+              <Text>Welcome!</Text>
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
 
   );
 }
 
 const styles = StyleSheet.create({
-  safeContainer: { flex: 1, backgroundColor: 'lightcoral' },
+  safeContainer: {
+    flex: 1,
+    ...Platform.select({
+      ios: {
+
+        backgroundColor: 'plum'
+      },
+      android: {
+        backgroundColor: 'black'
+      }
+
+    }),
+  },
   container: {
     flex: 1,
     backgroundColor: 'plum',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   box: {
     backgroundColor: 'lightblue',
